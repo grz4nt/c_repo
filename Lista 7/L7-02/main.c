dc17c335bf0f2804562b833facce0588f962165b
Grzegorz Antkowicz <grz4nt@gmail.com>
2023-12-05 22:10:32 +0100

    .

---
+++ Lista 7/L7-02/main.c
@@ -0,0 +1,5 @@
+﻿/*Napisz program, w którym do zdefiniowanej tablicy dwuwymiarowej o rozmiarach 5x5
+wpisujemy z klawiatury liczby całkowite z zakresu < -5, 10) bez 0. Program ma
+wyświetlić tablicę na ekranie(każdy element na 3 polach), a następnie obliczyć sumę
+wybranej przez użytkownika(w trakcie działania programu) kolumny.
+*/
---
+++ Lista 7/L7-04/main.c
@@ -0,0 +1,6 @@
+﻿/*
+Napisz program, w którym tworzona jest macierz górnotrójkątną o rozmiarze (5x5)
+wypełniona losowymi liczbami rzeczywistymi zakresu <0,10>. Zawartość tablicy ma być
+wyprowadzona na ekran (każdy element ma być wypisany na 5 polach z dwoma
+miejscami po przecinku).
+*/
